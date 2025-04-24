91eee0b3597af1e8be00990c58a91c4dfc4dde50
wyatt987 <526wbedor@frhsd.com>
2025-04-24 09:29:29 -0400

    fix 3200

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 /*Left button */
 let buttonhova2 = document.getElementById('buttonhova2')
 let icon = document.getElementById('icon')
-let choice
+let choice = null
+let eightSplits = document.getElementById('800mSplits')
+let eightSplits2 = document.getElementById('800mSplits2')
+let eightSplits3 = document.getElementById('800mSplits3')
+let mileSplits = document.getElementById('mileSplits')
+let mileSplits2 = document.getElementById('mileSplits2')
+let mileSplits3 = document.getElementById('mileSplits3')
+let twoMileSplits = document.getElementById('twoMileSplits')
+let twoMileSplits2 = document.getElementById('twoMileSplits2')
+let twoMileSplits3 = document.getElementById('twoMileSplits3')
+
 function toggle() {
     buttonhova2.classList.toggle('clicked')
     console.log('800m')
-    choice = 800
-    icon.classList.add('animate');
+    choice = '800'
+    icon.classList.add('in-n-out');
 }
 
 /*middle button */
@@ -15,8 +25,8 @@ let buttonhova3 = document.getElementById('buttonhova3')
 function toggle2() {
     buttonhova3.classList.toggle('clicked')
     console.log('1600m')
-    choice = 1600
-    icon.classList.add('animate');
+    choice = '1600'
+    icon.classList.add('in-n-out');
 }
 /*Right button */
 
@@ -25,17 +35,17 @@ let buttonhova4 = document.getElementById('buttonhova4')
 function toggle3() {
     buttonhova4.classList.toggle('clicked')
     console.log('3200m')
-    choice = 3200
+    choice = '3200'
     icon.classList.add('in-n-out');
 }
 let timeInput = document.getElementById('timeInput')
 
 function inputedTime() {
-    if(choice === '800m') {
+    if(choice === '800') {
         calculateSplits800()
-    } else if(choice === '1600m') {
+    } else if(choice === '1600') {
         calculateSplits1600()
-    } else if(choice === '3200m') {
+    } else if(choice === '3200') {
         calculateSplits3200()
     }
 }
@@ -51,10 +61,10 @@ function calculateSplits800() {
 
     totalTime = inputFieldM + inputFieldS;
 
-    displaySplits();
+    displaySplits800();
 }
 
-function displaySplits() {
+function displaySplits800() {
     for (let t = 1; t <= 4; t++) {
         let distance = t * 200; 
         let split = (totalTime / 4) * t; 
@@ -69,6 +79,96 @@ function displaySplits() {
             element.innerText = splitText;
         }
     }
+    eightSplits.classList.remove('d-none')
+    eightSplits2.classList.remove('d-none')
+    eightSplits3.classList.remove('d-none')
 }
 
+function resetButton800() {
+    buttonhova2.classList.toggle('clicked')
+    eightSplits.classList.add('d-none')
+    eightSplits2.classList.add('d-none')
+    eightSplits3.classList.add('d-none')
+    totalTime = 0;
 
+}
+
+function calculateSplits1600() {
+    let inputFieldM = document.getElementById('timeInputM').value;
+    let inputFieldS = document.getElementById('timeInputS').value;
+
+    inputFieldM = Number(inputFieldM) * 60;
+    inputFieldS = Number(inputFieldS);
+
+    totalTime = inputFieldM + inputFieldS;
+
+    displaySplits1600();
+}
+
+function displaySplits1600() {
+    for (let t = 1; t <= 4; t++) {
+        let distance = t * 400; 
+        let split = (totalTime / 4) * t; 
+        let splitMin = Math.floor(split / 60); // Convert back to seconds
+        let splitSec = Math.floor(split % 60).toString();  //Round
+        let splitText = `${splitMin}m ${splitSec}s`;
+
+//Match each with time
+        let elementId = `1600m${distance}`;
+        let element = document.getElementById(elementId);
+        if (element) {
+            element.innerText = splitText;
+        }
+    }
+    mileSplits.classList.remove('d-none')
+    mileSplits2.classList.remove('d-none')
+    mileSplits3.classList.remove('d-none')
+}
+
+function resetButton1600() {
+    buttonhova2.classList.toggle('clicked')
+    mileSplits.classList.add('d-none')
+    mileSplits2.classList.add('d-none')
+    mileSplits3.classList.add('d-none')
+    totalTime = 0;
+}
+
+function calculateSplits3200() {
+    let inputFieldM = document.getElementById('timeInputM').value;
+    let inputFieldS = document.getElementById('timeInputS').value;
+
+    inputFieldM = Number(inputFieldM) * 60;
+    inputFieldS = Number(inputFieldS);
+
+    totalTime = inputFieldM + inputFieldS;
+
+    displaySplits3200();
+}
+
+function displaySplits3200() {
+    for (let t = 1; t <= 4; t++) {
+        let distance = t * 800; 
+        let split = (totalTime / 4) * t; 
+        let splitMin = Math.floor(split / 60); // Convert back to seconds
+        let splitSec = Math.floor(split % 60).toString();  //Round
+        let splitText = `${splitMin}m ${splitSec}s`;
+
+//Match each with time
+        let elementId = `3200m${distance}`;
+        let element = document.getElementById(elementId);
+        if (element) {
+            element.innerText = splitText;
+        }
+    }
+    twoMileSplits.classList.remove('d-none')
+    twoMileSplits2.classList.remove('d-none')
+    twoMileSplits3.classList.remove('d-none')
+}
+
+function resetButton3200() {
+    buttonhova2.classList.toggle('clicked')
+    twoMileSplits.classList.add('d-none')
+    twoMileSplits2.classList.add('d-none')
+    twoMileSplits3.classList.add('d-none')
+    totalTime = 0;
+}
